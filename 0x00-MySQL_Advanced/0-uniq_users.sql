6ecdbb3e1241c32c6a1c1866708ef5a131c99baf
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2024-08-21 08:59:49 +0100

    We are all unique

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,17 @@
+-- Create Database called holberton if not exists
+CREATE DATABASE IF NOT EXISTS holberton;
+
+-- Use the database
+USE holberton;
+
+-- Create the table 'users' with specified attributes
+CREATE TABLE IF NOT EXISTS users (
+    -- 'id' column: an integer that never allows null values, auto-increments, and serves as the primary key
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+
+    -- 'email' column: a string that never allows null values, with a maximum length of 255 characters and must be unique
+    email VARCHAR(255) NOT NULL UNIQUE,
+
+    -- 'name' column: a string with a maximum length of 255 characters
+    name VARCHAR(255)
+);
