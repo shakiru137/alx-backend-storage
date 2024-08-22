b798dc205bd484ad038c6f6b421cfa9cf77edc5d
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2024-08-22 12:49:58 +0000

    Updated

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,20 +1,8 @@
--- Create Database called holberton if not exists
-CREATE DATABASE IF NOT EXISTS holberton;
-
--- Use the database
-USE holberton;
-
--- Create the table 'users' with specified attributes
+-- creates a table users
 CREATE TABLE IF NOT EXISTS users (
-	-- 'id' column: an integer that never allows null values, auto-increments, and serves as the primary key
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
-	-- 'email' column: a string that never allows null values, with a maximum length of 255 characters and must be unique
-	email VARCHAR(255) NOT NULL UNIQUE,
-
-	-- 'name' column: a string with a maximum length of 255 characters
-	name VARCHAR(255),
-
-	-- 'country' column: a string with enumuration of US, CO, and TN with US set as default
-	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+    id INT NOT NULL AUTO_INCREMENT, 
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL,
+    PRIMARY KEY (id)
 );
--- 0x00-MySQL_Advanced/README.md
+++ 0x00-MySQL_Advanced/README.md
@@ -1,4 +1,11 @@
-# 0x00. MySQL advanced
-## Back-end
-## SQL
-## MySQL
+# MySQL advanced
+
+# Learning Objectives
+
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+* How to create tables with constraints
+* How to optimize queries by adding indexes
+* What is and how to implement stored procedures and functions in MySQL
+* What is and how to implement views in MySQL
+* What is and how to implement triggers in MySQL
